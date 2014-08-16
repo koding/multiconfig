74e03f3a8e51985e4eb33e22d2a2dd28f3f7a63e
Cihangir SAVAS <cihangir@koding.com>
2014-08-15 22:27:49 -0700

    Multiconfig: enable embedded struct

--- multiconfig_test.go
+++ multiconfig_test.go
@@ -2,12 +2,24 @@ package multiconfig
 
 import "testing"
 
-type Server struct {
-	Name    string
-	Port    int
-	Enabled bool
-	Users   []string
-}
+type (
+	Server struct {
+		Name     string
+		Port     int
+		Enabled  bool
+		Users    []string
+		Postgres Postgres
+	}
+
+	// Postgres holds Postgresql database related configuration
+	Postgres struct {
+		Enabled           bool
+		Port              int
+		Hosts             []string
+		DBName            string
+		AvailabilityRatio float64
+	}
+)
 
 var (
 	testTOML = "testdata/config.toml"
@@ -35,10 +47,52 @@ func TestLoad(t *testing.T) {
 	}
 
 	if !s.Enabled {
-		t.Errorf("Enabled value is wrong: %s, want: %s", s.Port, true)
+		t.Errorf("Enabled value is wrong: %s, want: %s", s.Enabled, true)
 	}
 
 	if len(s.Users) != 2 {
 		t.Errorf("Users value is wrong: %s, want: %s", len(s.Users), 2)
+	}
+}
+
+func TestTomlEmbeddedStruct(t *testing.T) {
+	m := NewWithPath(testTOML)
+
+	s := &Server{}
+	if err := m.Load(s); err != nil {
+		t.Error(err)
+	}
+
+	// Explicitly state that Enabled should be true, no need to check
+	// `x == true` infact.
+	if s.Postgres.Enabled != true {
+		t.Error("Enabled should be true")
+	}
+
+	port := 5432
+	if s.Postgres.Port != port {
+		t.Errorf("Port value is wrong: %s, want: %s", s.Postgres.Port, port)
+	}
+
+	dbName := "configdb"
+	if s.Postgres.DBName != dbName {
+		t.Errorf("DBName is wrong: %s, want: %s", s.Postgres.DBName, dbName)
+	}
+
+	var availabilityRatio float64 = 8.23
+	if s.Postgres.AvailabilityRatio != availabilityRatio {
+		t.Errorf("AvailabilityRatio is wrong: %d, want: %d", s.Postgres.AvailabilityRatio, availabilityRatio)
+	}
+
+	hosts := []string{"192.168.2.1", "192.168.2.2", "192.168.2.3"}
+	if len(s.Postgres.Hosts) != 3 {
+		// do not continue testing if this fails, because others is depending on this test
+		t.Fatalf("Hosts len is wrong: %v, want: %v", s.Postgres.Hosts, hosts)
+	}
+
+	for i, host := range hosts {
+		if s.Postgres.Hosts[i] != host {
+			t.Fatalf("Hosts number %d is wrong: %v, want: %v", i, s.Postgres.Hosts[i], host)
+		}
 	}
 }
