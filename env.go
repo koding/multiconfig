f60ceabc4fc425ec99b014649b6d8b138de69b30
Cihangir SAVAS <cihangir@koding.com>
2014-08-17 03:14:54 -0700

    Multiconfig: change functions to methods

--- env.go
+++ env.go
@@ -19,7 +19,7 @@ func (e *EnvironmentLoader) Load(s interface{}) error {
 	strctName := strct.Name()
 
 	for _, field := range strct.Fields() {
-		if err := processField(strctName, field); err != nil {
+		if err := e.processField(strctName, field); err != nil {
 			return err
 		}
 	}
@@ -29,13 +29,13 @@ func (e *EnvironmentLoader) Load(s interface{}) error {
 
 // processField gets leading name for the env variable and combines the current
 // field's name and generates environemnt variable names recursively
-func processField(prefix string, field *structs.Field) error {
-	fieldName := strings.ToUpper(prefix) + "_" + strings.ToUpper(field.Name())
+func (e *EnvironmentLoader) processField(prefix string, field *structs.Field) error {
+	fieldName := generateFieldName(prefix, field)
 
 	switch field.Kind() {
 	case reflect.Struct:
 		for _, f := range field.Fields() {
-			if err := processField(fieldName, f); err != nil {
+			if err := e.processField(fieldName, f); err != nil {
 				return err
 			}
 		}
@@ -59,7 +59,23 @@ func (e *EnvironmentLoader) PrintEnvs(s interface{}) {
 	strctName := strct.Name()
 
 	for _, field := range strct.Fields() {
-		envName := strings.ToUpper(strctName) + "_" + strings.ToUpper(field.Name())
-		fmt.Println("  ", envName)
+		e.printEnvVariables(strctName, field)
 	}
+}
+
+func (e *EnvironmentLoader) printEnvVariables(prefix string, field *structs.Field) {
+	fieldName := generateFieldName(prefix, field)
+
+	switch field.Kind() {
+	case reflect.Struct:
+		for _, f := range field.Fields() {
+			e.printEnvVariables(fieldName, f)
+		}
+	default:
+		fmt.Println("  ", fieldName)
+	}
+}
+
+func generateFieldName(prefix string, field *structs.Field) string {
+	return strings.ToUpper(prefix) + "_" + strings.ToUpper(field.Name())
 }
