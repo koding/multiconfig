d23ab53b038f8f8768c57fdf437362da12d5876b
Cihangir SAVAS <cihangir@koding.com>
2014-08-18 12:13:33 -0700

    Multiconfig: generate environment variable names recursively

--- env.go
+++ env.go
@@ -30,7 +30,7 @@ func (e *EnvironmentLoader) Load(s interface{}) error {
 // processField gets leading name for the env variable and combines the current
 // field's name and generates environemnt variable names recursively
 func (e *EnvironmentLoader) processField(prefix string, field *structs.Field) error {
-	fieldName := strings.ToUpper(prefix) + "_" + strings.ToUpper(field.Name())
+	fieldName := e.generateFieldName(prefix, field)
 
 	switch field.Kind() {
 	case reflect.Struct:
@@ -59,7 +59,26 @@ func (e *EnvironmentLoader) PrintEnvs(s interface{}) {
 	strctName := strct.Name()
 
 	for _, field := range strct.Fields() {
-		envName := strings.ToUpper(strctName) + "_" + strings.ToUpper(field.Name())
-		fmt.Println("  ", envName)
+		e.printField(strctName, field)
 	}
+}
+
+// printField prints the field of the config struct for the flag.Usage
+func (e *EnvironmentLoader) printField(prefix string, field *structs.Field) {
+	fieldName := e.generateFieldName(prefix, field)
+
+	switch field.Kind() {
+	case reflect.Struct:
+		for _, f := range field.Fields() {
+			e.printField(fieldName, f)
+		}
+	default:
+		fmt.Println("  ", fieldName)
+	}
+}
+
+// generateFieldName generates the fiels name conbined with the prefix and the
+// struct's field name
+func (e *EnvironmentLoader) generateFieldName(prefix string, field *structs.Field) string {
+	return strings.ToUpper(prefix) + "_" + strings.ToUpper(field.Name())
 }
