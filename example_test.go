aa1b51077df5fba282b16dda045f2caf8ce349e9
Cihangir SAVAS <cihangir@koding.com>
2014-08-19 00:12:30 -0700

    Multiconfig:add toml config example

--- example_test.go
+++ example_test.go
@@ -11,14 +11,36 @@ type S struct {
 }
 
 func ExampleEnvironmentLoader() {
+	// Assume those values defined before running the Loader
 	os.Setenv("S_HOST", "golang.org")
 	os.Setenv("S_PORT", "80")
+
+	// Instantiate loader
 	l := &EnvironmentLoader{}
+	s := &S{}
 	err := l.Load(s)
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Here is our little config")
+	fmt.Println("Host-->", s.Host)
+	fmt.Println("Port-->", s.Host)
+}
+
+func ExampleTOMLLoader() {
+	const path = "/path/to/config"
+
+	// Instantiate loader
+	l := &TOMLLoader{Path: path}
+
+	s := &S{}
+	err := l.Load(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Here is our little config")
 	fmt.Println("Host-->", s.Host)
 	fmt.Println("Port-->", s.Host)
 }
