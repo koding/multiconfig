5821e8831078e3e14f4c3057bc7e9f8481bc26a1
Cihangir SAVAS <cihangir@koding.com>
2014-09-10 00:32:54 -0700

    Multiconfig: another attempt at implementing validators

--- validator.go
+++ validator.go
@@ -1,5 +1,11 @@
 package multiconfig
 
+import (
+	"fmt"
+
+	"github.com/fatih/structs"
+)
+
 // Validator validates the config against any predefined rules, those predefined
 // rules should be given to this package. The implementer will be responsible
 // about the logic
@@ -25,4 +31,32 @@ func NewValidator(validators ...Validator) *DefaultValidator {
 	return &DefaultValidator{
 		Validators: validators,
 	}
+}
+
+type RequiredValidator struct {
+	RequiredTagName string
+}
+
+func (e *RequiredValidator) Validate(s interface{}) error {
+	if e.RequiredTagName == "" {
+		e.RequiredTagName = "required"
+	}
+
+	requiredFields := []string{}
+	for _, field := range structs.Fields(s) {
+		defaultVal := field.Tag(e.RequiredTagName)
+		if defaultVal == "" {
+			continue
+		}
+
+		if field.IsZero() {
+			requiredFields = append(requiredFields, field.Name())
+		}
+	}
+
+	if len(requiredFields) == 0 {
+		return nil
+	}
+
+	return fmt.Errorf("Field(s) [%v] are required", requiredFields)
 }
