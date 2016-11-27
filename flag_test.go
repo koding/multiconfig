4b266a09dd68a25d83a44efaec171e7e6de7ca36
okzk <okazaki@okzk.com>
2016-11-27 22:42:40 +0900

    allow the usage defined on tag

--- flag_test.go
+++ flag_test.go
@@ -120,6 +120,26 @@ func TestCustomUsageFunc(t *testing.T) {
 	}
 }
 
+func TestCustomUsageTag(t *testing.T) {
+	const usageMsg = "foobar help"
+	strt := struct {
+		Foobar string `flagUsage:"foobar help"`
+	}{}
+	m := FlagLoader{}
+	err := m.Load(&strt)
+
+	if err != nil {
+		t.Fatalf("Unable to load struct: %s", err)
+	}
+	f := m.flagSet.Lookup("foobar")
+	if f == nil {
+		t.Fatalf("Flag foobar is not set")
+	}
+	if f.Usage != usageMsg {
+		t.Fatalf("usage message was %q, expected %q", f.Usage, usageMsg)
+	}
+}
+
 // getFlags returns a slice of arguments that can be passed to flag.Parse()
 func getFlags(t *testing.T, structName, prefix string) []string {
 	if structName == "" {
